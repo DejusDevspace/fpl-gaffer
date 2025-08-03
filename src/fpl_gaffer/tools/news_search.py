551cb9a8ab94118980fc8e3e69557437eca6df70
Deju Adejo <ojomideju2003@gmail.com>
2025-08-04 00:28:16 +0100

    news searcher init func

--- src/fpl_gaffer/tools/news_search.py
+++ src/fpl_gaffer/tools/news_search.py
@@ -1,3 +1,19 @@
+from langchain_community.tools import TavilySearchResults
+from langchain_community.tools.ddg_search import DuckDuckGoSearchRun
+from fpl_gaffer.settings import settings
+
+
 class FPLNewsSearcher:
     def __init__(self):
-        pass
+        tavily_api_key = settings.TAVILY_API_KEY
+        if tavily_api_key:
+            self.tavily = TavilySearchResults(
+                max_results=5,
+                search_depth="advanced",
+                api_wrapper=tavily_api_key
+            )
+        else:
+            self.tavily = None
+
+        # Alternative search tool using DuckDuckGo (if tavily is not available)
+        self.ddg = DuckDuckGoSearchRun()
