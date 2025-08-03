7b90a7f2158eb78611eaf98da4d486e7f77ab691
Deju Adejo <ojomideju2003@gmail.com>
2025-08-03 23:55:21 +0100

    created settings config file

---
+++ src/fpl_gaffer/settings.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    """Application settings."""
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+
+    # Application settings
+    app_name: str = "FPL Gaffer"
+
+    # FPL API settings
+    fpl_api_base_url: str = "https://fantasy.premierleague.com/api"
+
+settings = Settings()
