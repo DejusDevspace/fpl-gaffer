444a3352f926434cde656544a7b6e634726ffce3
Deju Adejo <ojomideju2003@gmail.com>
2025-08-03 23:17:44 +0100

    bootsrap data boilerplate func

--- src/fpl_gaffer/tools/fpl_api.py
+++ src/fpl_gaffer/tools/fpl_api.py
@@ -1,7 +1,12 @@
 import httpx
+from typing import Dict
 
 
 class FPLOfficialAPI:
     def __init__(self):
         self.base_url = "https://fantasy.premierleague.com/api"
         self.session = httpx.AsyncClient()
+
+    async def get_bootstrap_data(self) -> Dict:
+        """Get basic FPL data including gameweeks, teams, players, chips..."""
+        pass
