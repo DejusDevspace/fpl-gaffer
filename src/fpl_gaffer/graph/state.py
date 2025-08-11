261b2f56f89ee11cf42173b9bdc1e5bda644e9f0
Deju Adejo <ojomideju2003@gmail.com>
2025-08-11 23:01:52 +0100

    added state default state variables

--- src/fpl_gaffer/graph/state.py
+++ src/fpl_gaffer/graph/state.py
@@ -1,7 +1,23 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional, Literal, Dict, List
+from datetime import datetime
+from langchain.schema import Document
 
 
 class WorkflowState(BaseModel):
-    """State parameters for the fpl gaffer workflow."""
+    """State for the fpl gaffer workflow."""
 
-    pass
+    # Core state variables
+    stage: Literal["collector", "analyzer", "decision"] = "collector"
+    gameweek: Optional[int] = None
+    deadline: Optional[datetime] = None
+
+    # Data collector agent variables
+    fpl_api_data: Dict = Field(default_factory=dict)
+    news_search_data: List[Document] = Field(default_factory=list)
+
+    # Data analyzer agent variables
+    # TODO: Determine analyzer agent output structure
+
+    # Decision agent variables
+    # TODO: Determine decision agent output structure
