dcc0ce651582dec5c4f72100e1662b9ceb54dfcf
Deju Adejo <ojomideju2003@gmail.com>
2025-08-12 12:22:33 +0100

    refactor: restructured tools directory

--- src/fpl_gaffer/tools/__init__.py
+++ src/fpl_gaffer/tools/__init__.py
@@ -1,5 +1,5 @@
-from .fpl_api import FPLOfficialAPI
-from .news_search import FPLNewsSearcher
+from .data_collector.fpl_api import FPLOfficialAPI
+from .data_collector.news_search import FPLNewsSearcher
 
 __all__ = [
     "FPLOfficialAPI",
