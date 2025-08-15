52dc9e32cb73f5897454e97440f389396b9ae579
Deju Adejo <ojomideju2003@gmail.com>
2025-08-16 00:17:44 +0100

    created main function boilerplate

--- src/fpl_gaffer/tools/data_collector/user_data.py
+++ src/fpl_gaffer/tools/data_collector/user_data.py
@@ -1,3 +1,13 @@
+from fpl_gaffer.settings import settings
+from httpx import AsyncClient
+from typing import Dict
+from fpl_gaffer.core.exceptions import FPLAPIError
+
 class FPLUserDataExtractor:
     def __init__(self):
+        self.base_url = settings.fpl_api_base_url
+        self.session = AsyncClient()
+
+    async def extract_user_data(self, manager_id: int) -> Dict:
+        """Get user data from the FPL API."""
         pass
