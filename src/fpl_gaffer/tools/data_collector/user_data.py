35ddd7d087a1664c3d2be40d7debf347d7656696
Deju Adejo <ojomideju2003@gmail.com>
2025-08-16 00:59:18 +0100

    checkpoint: manager data extraction

--- src/fpl_gaffer/tools/data_collector/user_data.py
+++ src/fpl_gaffer/tools/data_collector/user_data.py
@@ -1,6 +1,6 @@
 from fpl_gaffer.settings import settings
 from httpx import AsyncClient
-from typing import Dict
+from typing import Dict, Optional
 from fpl_gaffer.core.exceptions import FPLAPIError
 
 class FPLUserDataExtractor:
@@ -10,4 +10,18 @@ class FPLUserDataExtractor:
 
     async def extract_user_data(self, manager_id: int) -> Dict:
         """Get user data from the FPL API."""
-        pass
+        # Get manager data
+        manager_data = await self.get_manager_data(manager_id)
+        if not manager_data:
+            return {}
+
+    async def get_manager_data(self, manager_id: int) -> Dict:
+        """Get basic manager data from the FPL API."""
+        try:
+            response = await self.session.get(f"{self.base_url}/entry/{manager_id}/")
+            response.raise_for_status()
+            return response.json()
+        except Exception as e:
+            raise FPLAPIError(
+                f"Failed to fetch manager data for manager with ID: {manager_id}:\n {e}"
+            ) from e
