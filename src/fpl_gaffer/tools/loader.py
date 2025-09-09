c3331172a77ec2bb7f95a5712e7e2c03f568d142
Deju Adejo <ojomideju2003@gmail.com>
2025-09-09 15:21:25 +0100

    create tool export boilerplate

---
+++ src/fpl_gaffer/tools/loader.py
@@ -0,0 +1,9 @@
+from typing import List
+from langchain.tools import Tool
+
+
+# TODO: Import tools from tool module and create 'create_tools' function
+def create_tools() -> List[Tool]:
+    """Create and return a list of tools for the FPL Gaffer."""
+    tools = []
+    return tools
